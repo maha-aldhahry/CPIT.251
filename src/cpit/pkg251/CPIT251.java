51deda0c61e3c15fc045d69457fb22f439d04555
Maha Aldhahry <maha.ffd2@gmail.com>
2023-09-04 12:08:18 +0300

    First Commit!

---
+++ src/cpit/pkg251/CPIT251.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cpit.pkg251;
+
+/**
+ *
+ * @author mahaf
+ */
+public class CPIT251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
