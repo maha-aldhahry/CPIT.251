c28eb2800c2b0736f19b2344b88a04b56ba883fd
RaghadAlzahraniii <144053748+RaghadAlzahraniii@users.noreply.github.com>
2023-09-11 20:00:28 +0300

    Update CPIT251.java

--- src/cpit/pkg251/CPIT251.java
+++ src/cpit/pkg251/CPIT251.java
@@ -16,6 +16,7 @@ public class CPIT251 {
     public static void main(String[] args) {
         System.out.println("hello world");
                System.out.println("Shahad Ahmed Alshmrani");
+                    System.out.println("Raghad Abdulaziz Alzahrani");
     }
     
 }
